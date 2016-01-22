c9aacf869a9517d468880da82ab2497c13861fd5
Steve David <steve@MacBook-Pro-de-Link.local>
2016-01-22 15:48:14 +0100

    added config.yml handling

---
+++ src/class/Config.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../vendor/symfony/yaml/Yaml.php';
+
+use Symfony\Component\Yaml\Yaml;
+
+class Config
+{
+    const CONFIG_PATH = '../config';
+    const CONFIG_FILE = 'config.yml';
+
+    public function __construct()
+    {
+        $file = self::CONFIG_PATH . '/' . self::CONFIG_FILE;
+
+        $config = Yaml::parse(file_get_contents($file));
+
+        foreach($config as $option => $value) {
+            $this->$option = $value;
+        }
+    }
+}
